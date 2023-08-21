727d18f3dea8d38546388c0f9bd2efd81584c3a0
shubh-00 <131482908+shubh-00@users.noreply.github.com>
2023-08-21 16:19:19 +0530

    first commit

---
+++ src/modals/EditTask.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const EditTaskPopup = ({ modal, toggle, updateTask, taskObj }) => {
+    const [taskName, setTaskName] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        if (name === 'taskName') {
+            setTaskName(value);
+        } else {
+            setDescription(value);
+        }
+    };
+
+    useEffect(() => {
+        setTaskName(taskObj.Name);
+        setDescription(taskObj.Description);
+    }, [taskObj]); // Add taskObj as a dependency to useEffect
+
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        const updatedTask = {
+            Name: taskName,
+            Description: description
+        };
+        updateTask(updatedTask);
+    };
+
+    return (
+        <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader toggle={toggle}>Update Task</ModalHeader>
+            <ModalBody>
+                <div className="form-group">
+                    <label>Task Name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={taskName}
+                        onChange={handleChange}
+                        name="taskName"
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Description</label>
+                    <textarea
+                        rows="5"
+                        className="form-control"
+                        value={description}
+                        onChange={handleChange}
+                        name="description"
+                    />
+                </div>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="primary" onClick={handleUpdate}>
+                    Update
+                </Button>{' '}
+                <Button color="secondary" onClick={toggle}>
+                    Cancel
+                </Button>
+            </ModalFooter>
+        </Modal>
+    );
+};
+
+export default EditTaskPopup;
